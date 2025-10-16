5041ad86a7fd71163ce03834ff6763bb9dc0a028
roeigold2002 <125883941+roeigold2002@users.noreply.github.com>
2025-10-16 09:37:42 +0300

    Update install_wifi_driver.ps1

--- install_wifi_driver.ps1
+++ install_wifi_driver.ps1
@@ -15,3 +15,9 @@ Write-Host "Installing driver sp155883.exe..."
 Start-Process -FilePath "C:\Drivers\sp155883.exe" -ArgumentList "/s" -Wait
 
 Write-Host "All drivers installed successfully."
+
+# --- הסרת העדכון KB5065848 ---
+Write-Host "Removing update package KB5065848..."
+Start-Process -FilePath "Dism.exe" -ArgumentList "/online", "/remove-package", "/packagepath:C:\Windows\servicing\packages\package_for_kb5065848~31bf3856ad364e35~amd64~~26100.5058.1.0.mum" -Wait -NoNewWindow
+
+Write-Host "Update KB5065848 removed successfully."
