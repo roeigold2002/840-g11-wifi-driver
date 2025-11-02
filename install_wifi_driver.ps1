fb8042f0277b377c395e1d42e820e206502ba70a
roeigold2002 <125883941+roeigold2002@users.noreply.github.com>
2025-11-02 15:27:20 +0200

    Update install_wifi_driver.ps1

--- install_wifi_driver.ps1
+++ install_wifi_driver.ps1
@@ -7,6 +7,7 @@ Write-Host "Downloading drivers..."
 Invoke-WebRequest -Uri "https://ftp.hp.com/pub/softpaq/sp157001-157500/sp157284.exe" -OutFile "C:\Drivers\sp157284.exe"
 Invoke-WebRequest -Uri "https://ftp.hp.com/pub/softpaq/sp155501-156000/sp155883.exe" -OutFile "C:\Drivers\sp155883.exe"
 Invoke-WebRequest -Uri "https://ftp.hp.com/pub/softpaq/sp153501-154000/sp153667.exe" -OutFile "C:\Drivers\sp153667.exe"
+Invoke-WebRequest -Uri "https://ftp.hp.com/pub/softpaq/sp158001-158500/sp158031.exe" -OutFile "C:\Drivers\sp158031.exe"
 
 # --- התקנת דרייברים (silent) ---
 Write-Host "Installing driver sp157284.exe..."
@@ -18,5 +19,12 @@ Start-Process -FilePath "C:\Drivers\sp155883.exe" -ArgumentList "/s" -Wait
 Write-Host "Installing driver sp153667.exe..."
 Start-Process -FilePath "C:\Drivers\sp153667.exe" -ArgumentList "/s" -Wait
 
+Write-Host "Installing driver sp158031.exe..."
+Start-Process -FilePath "C:\Drivers\sp158031.exe" -ArgumentList "/s" -Wait
+
 Write-Host "All drivers installed successfully."
 
+# --- הודעה על ריסטארט וביצוע ---
+Write-Host "The system will restart in 10 seconds to apply the driver changes..."
+Start-Sleep -Seconds 10
+Restart-Computer
